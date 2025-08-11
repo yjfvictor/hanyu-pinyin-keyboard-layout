03f01decc5ab38002439ddbd67fc4c4da93fb9dc
Victor Yeh <yjfvictor@users.noreply.github.com>
2025-08-12 02:04:15 +1000

    Modify the version number and add installation script
    增加版本号与安装脚本

---
+++ install.bat
@@ -0,0 +1,52 @@
+:: This is free and unencumbered software released into the public domain.
+::
+:: Anyone is free to copy, modify, publish, use, compile, sell, or
+:: distribute this software, either in source code form or as a compiled
+:: binary, for any purpose, commercial or non-commercial, and by any
+:: means.
+::
+:: In jurisdictions that recognize copyright laws, the author or authors
+:: of this software dedicate any and all copyright interest in the
+:: software to the public domain. We make this dedication for the benefit
+:: of the public at large and to the detriment of our heirs and
+:: successors. We intend this dedication to be an overt act of
+:: relinquishment in perpetuity of all present and future rights to this
+:: software under copyright law.
+::
+:: THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+:: EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+:: MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+:: IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+:: OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+:: ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+:: OTHER DEALINGS IN THE SOFTWARE.
+::
+:: For more information, please refer to <https://unlicense.org>
+
+@echo off
+
+if "%processor_architecture%" == "AMD64" goto AMD64
+if "%processor_architecture%" == "IA64" goto IA64
+if "%processor_architecture%" == "ARM64" goto ARM64
+if "%processor_architecture%" == "x86" goto x86
+
+:AMD64
+copy amd64\KBDCNPY.dll C:\Windows\System32\
+copy wow64\KBDCNPY.dll C:\Windows\SysWOW64\
+goto commonexit
+
+:IA64
+echo IA64
+copy ia64\KBDCNPY.dll C:\Windows\System32\
+goto commonexit
+
+:ARM64
+echo Not supported on ARM64
+goto commonexit
+
+:x86
+copy i386\KBDCNPY.dll C:\Windows\System32\
+goto commonexit
+
+:commonexit
+regedit /s 00000804.reg
